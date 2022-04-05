3247fa4850344946b91eaf0f13c13d10722125a0
Bianca Mota <biancamota1209@gmail.com>
2022-04-05 09:42:18 -0300

    feat: add read products

---
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-read',
+  templateUrl: './product-read.component.html',
+  styleUrls: ['./product-read.component.css']
+})
+export class ProductReadComponent implements OnInit {
+
+  products: Product[]; 
+  displayedColumns = ['id', 'name', 'price', 'action'];
+
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.productService.read().subscribe(products => {
+      this.products = products;
+      console.log(products);
+    })
+  }
+
+  deleteProduct(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage('Cadastro excluído com sucesso');
+    });
+  }
+
+}
