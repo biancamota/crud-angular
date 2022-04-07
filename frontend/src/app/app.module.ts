8e6eda1d5b4a50174278c6c414cb9040f961ed92
Bianca Mota <biancamota1209@gmail.com>
2022-04-07 09:47:20 -0300

    build: add sweetalert2

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -24,6 +24,7 @@ import { ProductReadComponent } from './components/product/product-read/product-
 import { MatTableModule } from '@angular/material/table';
 import localePt  from "@angular/common/locales/pt";
 import { registerLocaleData } from "@angular/common";
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 registerLocaleData(localePt);
 
@@ -53,7 +54,8 @@ registerLocaleData(localePt);
     MatInputModule,
     MatFormFieldModule,
     FormsModule,
-    MatTableModule
+    MatTableModule,
+    SweetAlert2Module.forRoot()
   ],
   providers: [{
     provide: LOCALE_ID,
